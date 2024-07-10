41794ab0eb274a900bb86c9a4b8692048bb632c9
mostafanagy0 <mostafanagy855@gmail.com>
2024-07-10 14:18:18 -0700

    add cubit to auth

--- lib/core/routes/App_Router.dart
+++ lib/core/routes/App_Router.dart
@@ -1,7 +1,10 @@
+import 'package:dalel/core/services/service_locator.dart';
+import 'package:dalel/feature/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
 import 'package:dalel/feature/auth/presentation/views/signIn_view.dart';
 import 'package:dalel/feature/auth/presentation/views/signUp_View.dart';
 import 'package:dalel/feature/on_bording/presentation/views/on_bording_view.dart';
 import 'package:dalel/feature/splash/presentation/views/splash%20_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
@@ -16,11 +19,17 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: "/signUp",
-      builder: (context, state) => const SignUpView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignUpView(),
+      ),
     ),
     GoRoute(
       path: "/signIn",
-      builder: (context, state) => const SignInView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignInView(),
+      ),
     ),
   ],
 );
--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,7 +1,9 @@
 import 'package:dalel/core/database/cashe/cash_helper.dart';
+import 'package:dalel/feature/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
+  getIt.registerSingleton<AuthCubit>(AuthCubit());
 }
